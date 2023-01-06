de0d5a7fd412ad1c7333925f5a68a3246173536f
Shiniri <marie_bauer1@gmx.de>
2023-01-06 21:46:35 +0100

    Added two useful functions

--- database_tools.py
+++ database_tools.py
@@ -35,3 +35,26 @@ def execute_query(connection, query, type):
             return result
         except Error as e:
             print(e)
+
+
+def add_word(database_connection, information):
+    add_word_query = f"""
+        INSERT INTO loanwords (hanzi, pinyin, ipa, original_form, eng_translation, description)
+        VALUES (\"{information[0]}\", 
+                \"{information[1]}\",
+                \"{information[2]}\",
+                \"{information[3]}\",
+                \"{information[4]}\",
+                \"{information[5]}\");
+        """
+    execute_query(database_connection, add_word_query, 'commit')
+
+
+def update_txt_from_database(database_connection):
+    with open('loanwords.txt', 'w', encoding='utf8') as file:
+        get_words_query = 'SELECT * FROM loanwords;'
+        row_list = execute_query(database_connection, get_words_query, 'read')
+        for word in row_list:
+            for column in word:
+                file.write(str(column) + ' ')
+            file.write('\n')
--- main.py
+++ main.py
@@ -1,61 +1,13 @@
-#
-#   TO_DO:
-#       - add 'original form'-column
-#       - make 'kafei' and 'xianzai' nicer
-#
-
-
-from tkinter import *
-from tkinter import ttk
-
 
 import database_tools
 
 
-class Database_Window(Tk):
-
-    def __init__(self):
-        super().__init__()
-
-        # database #
-        self.database_connection = database_tools.create_connection('loanwords.db')
-        self.listbox_content = self.get_listbox_contents()
-
-        # GUI #
-        self.geometry('300x400')
-        self.resizable(0, 0)
-        self.title('Chinese Loanwords')
-        self.create_widgets()
-
-    def create_widgets(self):
-        # menubar #
-        menubar = Menu(self)
-        menubar.add_command(label='Language')
-        menubar.add_command(label='Help')
-        menubar.add_command(label='Add Word')
-        self.config(menu=menubar)
-
-        # listbox #
-        loanword_listbox = Listbox(self, width=300)
-        for word in self.listbox_content:
-                loanword_listbox.insert(word[0], word[1:])
-        loanword_listbox.pack()
-
-        # searchbar #
-        search_bar = Entry(self, width=300)
-        search_bar.pack()
-
-        # information window #
-        information_window = Label(self, width=300)
-        information_window.pack()
-
-    def get_listbox_contents(self):
-        read_words_query = 'SELECT * FROM loanwords;'
-        columns = database_tools.execute_query(self.database_connection, read_words_query, 'read')
-
-        return columns
-        
-
 if __name__ == "__main__":
-    window = Database_Window()
-    window.mainloop()
+    connection = database_tools.create_connection('loanwords.db')
+
+    #                       #
+    #   Script from here    #
+    #                       #
+
+    # comment out if txt update not needed
+    database_tools.update_txt_from_database(connection)
