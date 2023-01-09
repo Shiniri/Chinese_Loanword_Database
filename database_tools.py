2d7a1d620a2fdb30d591f9a5bbfd53cd9456555a
Shiniri <marie_bauer1@gmx.de>
2023-01-09 14:18:50 +0100

    object orientation

--- database_tools.py
+++ database_tools.py
@@ -2,62 +2,83 @@ import sqlite3
 from sqlite3 import Error
 
 
-def create_connection(path):
+class Database_Tools():
+    def __init__(self):
+        self.database_path = 'chinese_loanword_database.db'
+        self.connection_to_database = self.create_connection()
+        self.loanword_txt = 'loanword.txt'
+    
 
-    connection = None
+    def create_connection(self):
 
-    try:
-        connection = sqlite3.connect(path)
-        print('Connection successful.')
-    except Error as e:
-        print(e)
-
-    return connection
-
-
-# type can be 'commit' or 'read'
-def execute_query(connection, query, type):
-
-    cursor = connection.cursor()
-
-    if type == 'commit':
         try:
-            cursor.execute(query)
-            connection.commit()
-            print('Success.')
-        except Error as e:
-            print(e)
-    elif type == 'read':
-        result = None
-        try:
-            cursor.execute(query)
-            result = cursor.fetchall()
-            return result
+            connection = sqlite3.connect(self.database_path)
+            print('Connection successful.')
         except Error as e:
             print(e)
 
-
-def add_word(database_connection, information):
-    add_word_query = f"""
-        INSERT INTO loanwords (hanzi, pinyin, ipa, original_form, eng_translation, description)
-        VALUES (\"{information[0]}\", 
-                \"{information[1]}\",
-                \"{information[2]}\",
-                \"{information[3]}\",
-                \"{information[4]}\",
-                \"{information[5]}\");
-        """
-    execute_query(database_connection, add_word_query, 'commit')
+        return connection
 
 
-def update_txt_from_database(database_connection):
-    with open('loanwords.txt', 'w', encoding='utf8') as file:
-        get_words_query = 'SELECT * FROM loanwords;'
-        row_list = execute_query(database_connection, get_words_query, 'read')
+    # type can be 'commit' or 'read'
+    def execute_query(self, query, type):
 
-        file.write('HANZI | PINYIN | IPA | ORIGINAL FORM | ENGLISH TRANSLATION | DESCRIPTION \n')
+        cursor = self.connection_to_database.cursor()
 
-        for word in row_list:
-            for column in word:
-                file.write(str(column) + ' ')
-            file.write('\n')
+        if type == 'commit':
+            try:
+                cursor.execute(query)
+                self.connection_to_database.commit()
+                print('Success.')
+            except Error as e:
+                print(e)
+        elif type == 'read':
+            result = None
+            try:
+                cursor.execute(query)
+                result = cursor.fetchall()
+                return result
+            except Error as e:
+                print(e)    
+
+
+    def create_table(self):
+        create_table_query = """
+            CREATE TABLE IF NOT EXISTS loanwords (
+                word_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                hanzi TEXT NOT NULL,
+                pinyin TEXT NOT NULL,
+                original_representation TEXT NOT NULL,
+                eng_translation TEXT NOT NULL,
+                donor TEXT NOT NULL,
+                category TEXT NOT NULL,
+                description TEXT NOT NULL)
+            """
+        self.execute_query(create_table_query, 'commit')
+
+
+    def add_word(self, information):
+        add_word_query = f"""
+            INSERT INTO loanwords (hanzi, pinyin, original_representation, eng_translation, donor, category, description)
+            VALUES (\"{information[0]}\", 
+                    \"{information[1]}\",
+                    \"{information[2]}\",
+                    \"{information[3]}\",
+                    \"{information[4]}\",
+                    \"{information[5]}\",
+                    \"{information[6]}\");
+            """
+        self.execute_query(add_word_query, 'commit')
+
+    
+    def update_txt_from_database(self):
+        with open(self.loanword_txt, 'w', encoding='utf8') as file:
+            get_words_query = 'SELECT * FROM loanwords;'
+            row_list = self.execute_query(get_words_query, 'read')
+
+            file.write('HANZI | PINYIN | DONOR | ORIGINAL FORM | ENGLISH TRANSLATION | CATEGORY | DESCRIPTION \n')
+
+            for word in row_list:
+                for column in word:
+                    file.write(str(column) + ' ')
+                file.write('\n')
--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-
 #
-#   TO_DO:
-#       - replace IPA with donor language column
-#
+#   TO-DO:
+#       . list categories somewhere:    physical_world, kinship, animals, body, food_drink, clothing_grooming, house,
+#                                       agriculture_vegetation, actions_technology, motion, possession, spatial_relations,
+#                                       quantity, time, sense_perception, emotions_values, cognition, speech_language,
+#                                       social_political_relations, warfare_hunting, law, religion_belief, modern_world,
+#                                       miscellaneous_function_words
 
-
-import database_tools
+from database_tools import *
 
 
 if __name__ == "__main__":
-    connection = database_tools.create_connection('loanwords.db')
+
+    db_interaction = Database_Tools()
+    db_interaction.create_table()
 
     #                       #
     #   Script from here    #
     #                       #
 
+    print('Hello World')
     
-    # comment out if txt update not needed
-    database_tools.update_txt_from_database(connection)
+    #               #
+    #   Script end  #
+    #               #
+
+    db_interaction.update_txt_from_database()
